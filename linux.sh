6a5b7144d6acabe74b6e2c515b96cf6de4d756d3
bluefish9981 <bluefish9981@gmail.com>
2019-10-21 13:07:15 -0500

    Something new

--- linux.sh
+++ linux.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+promptuname()
+{
+    echo -n "Enter the username and press [ENTER]: "
+    read uname
+    echo $uname > username
+}
+
+promptpasswd()
+{
+    echo "Enter the password (matching previously"
+    echo -n "entered user) and press [ENTER]: "
+    read -s passwd
+    echo $passwd > password
+}
+
 rmblacklist()
 {
     for pack in `cat blacklist`
@@ -8,21 +25,24 @@ rmblacklist()
 
 checkrelease() #still needs adjustments
 {
+    username=$(head -n 1 username)
     FILE1=/etc/os-release
     FILE2=/etc/lsb-release
     if test -f "$FILE1"
     then
-        if [$1 -eq 1]
+        if [ "$1" = "1" ]
         then
-            cat $FILE1 > ~/Desktop/release_info
+            cat $FILE1 > /home/$username/Desktop/release_info
+            echo "Saving to /home/$username/Desktop/release_info"
         else
             cat $FILE1
         fi
     elif test -f "$FILE2"
     then
-        if [$1 -eq 1]
+        if [ "$1" = "1" ]
         then
-            cat $FILE2 > ~/Desktop/release_info
+            cat $FILE2 > /home/$username/Desktop/release_info
+            echo "Saving to /home/$username/Desktop/release_info"
         else
             cat $FILE2
         fi
@@ -30,5 +50,20 @@ checkrelease() #still needs adjustments
         echo "Release infor not found"
     fi
 }
+
+menu()
+{
+    clear
+    echo "   ____      _               ____  _             _   
+  / ___|   _| |__   ___ _ __| __ )| |_   _  __ _| |_ 
+ | |  | | | | '_ \ / _ \ '__|  _ \| | | | |/ _\` | __|
+ | |__| |_| | |_) |  __/ |  | |_) | | |_| | (_| | |_ 
+  \____\__, |_.__/ \___|_|  |____/|_|\__, |\__,_|\__|
+       |___/                         |___/           "
+}
+menu
+#promptuname
+#promptpasswd
 #checkrelease
-rmblacklist
+#checkrelease 1
+#rmblacklist
