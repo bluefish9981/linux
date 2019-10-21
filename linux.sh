8727e745dca7b152bcb656f502d50548bb394aa7
bluefish9981 <bluefish9981@gmail.com>
2019-10-21 00:08:32 -0500

    Something new

--- linux.sh
+++ linux.sh
@@ -5,4 +5,30 @@ rmblacklist()
     echo "Uninstalling $pack  if found."
     done
 }
+
+checkrelease() #still needs adjustments
+{
+    FILE1=/etc/os-release
+    FILE2=/etc/lsb-release
+    if test -f "$FILE1"
+    then
+        if [$1 -eq 1]
+        then
+            cat $FILE1 > ~/Desktop/release_info
+        else
+            cat $FILE1
+        fi
+    elif test -f "$FILE2"
+    then
+        if [$1 -eq 1]
+        then
+            cat $FILE2 > ~/Desktop/release_info
+        else
+            cat $FILE2
+        fi
+    else
+        echo "Release infor not found"
+    fi
+}
+#checkrelease
 rmblacklist
