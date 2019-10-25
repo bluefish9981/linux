9727d0a6b8978341955a54ad20d4fb08a14f0a7c
bluefish9981 <56805041+bluefish9981@users.noreply.github.com>
2019-10-25 15:53:51 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 echo "Updating..."
-sudo apt-get -y update | grep "random bullshit"
+sudo apt-get update -y | grep "random bullshit"
 echo "Done"
 echo ""
 echo "Update done, upgrading(and full-upgrade) now..."
-sudo apt-get -y upgrade | grep "random bullshit"
-sudo apt-get -y full-upgrade | grep "random bullshit"
+sudo apt-get upgrade -y | grep "random bullshit"
+#sudo apt-get full-upgrade | grep "random bullshit"
 echo ""
 echo "Complete!"
 if [ "$1" = "1" ]
