68d476d8b4cb78085366f136218c378d19528049
bluefish9981 <56805041+bluefish9981@users.noreply.github.com>
2020-01-24 03:08:17 -0600

    Update checklists.sh

--- checklists.sh
+++ checklists.sh
@@ -5,4 +5,4 @@ xdg-open https://github.com/wxyfreedom/CyberPatriot/tree/master/LICENSE.md
 xdg-open https://github.com/caf203/cyberpatriot-debian8-hardening
 xdg-open https://docs.google.com/document/d/e/2PACX-1vTD-9VUBflKTb2S9hmVHN7XNFYWP843z1bGhPtY53xB3uXwgxYZCOmNKr-lbHtIg3LfZw4NeeIgIKoc/pub
 echo "Complete!"
-sleep 60
+sleep 5
